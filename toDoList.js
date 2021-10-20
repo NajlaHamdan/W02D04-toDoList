bd861443d66859bed067045d63a1ae9b211ed1f8
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-10-20 20:58:48 +0300

    task after prettier

--- toDoList.js
+++ toDoList.js
@@ -1,64 +1,72 @@
-const h1=document.createElement("h1");
-const body=document.querySelector("body");
-h1.innerHTML="to do list";
-h1.className="page-header";
+const h1 = document.createElement("h1");
+h1.innerHTML = "to do list";
+h1.className = "page-header";
+
+const body = document.querySelector("body");
+const toDoList = document.createElement("ul");
+
+body.className = "container";
+toDoList.className = "list-group";
+
 body.append(h1);
-body.className="container";
+
 ////////Delete button
 
-
-const toDoList=document.createElement("ul");
-toDoList.className="list-group";
-const tasks=["wake up","eat breakfast","code"];
+const tasks = ["wake up", "eat breakfast", "code"];
 body.append(toDoList);
 
-
-function renderList(){
-    toDoList.innerHTML ="";
-    tasks.forEach((element,index) => {
-        const items=document.createElement("li");
-        items.className="list-group-item";
-        const deleteButton=document.createElement("button");
-        deleteButton.innerHTML="Delete";
-        deleteButton.className="btn btn-danger";
-        const updateButton=document.createElement("button");
-        updateButton.innerHTML="Update";
-        updateButton.className="btn btn-info"
-        items.innerHTML=element;
-        items.appendChild(deleteButton);
-        items.appendChild(updateButton);
-        toDoList.append(items);
-        deleteButton.addEventListener('click',()=>{deleteItemsList(index)});
-        updateButton.addEventListener('click',()=>{updateItemsList(index)});
+function renderList() {
+  toDoList.innerHTML = "";
+  tasks.forEach((element, index) => {
+    const items = document.createElement("li");
+    items.className = "list-group-item";
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.className = "btn btn-danger";
+    const updateButton = document.createElement("button");
+    updateButton.innerHTML = "Update";
+    updateButton.className = "btn btn-info";
+    items.innerHTML = element;
+    items.appendChild(deleteButton);
+    items.appendChild(updateButton);
+    toDoList.append(items);
+    deleteButton.addEventListener("click", () => {
+      deleteItemsList(index);
     });
+    updateButton.addEventListener("click", () => {
+      updateItemsList(index);
+    });
+  });
 }
-
 renderList();
-////////////////Add button
-const addItem=document.createElement("button");
-addItem.innerHTML="add to list";
-addItem.className="btn btn-primary";
-body.append(addItem);
-addItem.addEventListener('click',addToList);
 
-function addToList(){
-    const inputValue = input.value;
+////////////////create and append Add button
+const addItem = document.createElement("button");
+addItem.innerHTML = "add to list";
+addItem.className = "btn btn-primary";
+addItem.addEventListener("click", addToList);
+body.append(addItem);
+
+function addToList() {
+  const inputValue = input.value;
+  if (inputValue.length != "") {
     tasks.push(inputValue);
     renderList();
+  }
 }
-
-const input=document.createElement("input");
-input.className="form"
-input.placeholder="to do";
+////////////////create and append input
+const input = document.createElement("input");
+input.className = "form";
+input.placeholder = "to do";
 body.append(input);
 
-function deleteItemsList(index){
-    tasks.splice(index,1);
-    renderList();
+function deleteItemsList(index) {
+  tasks.splice(index, 1);
+  renderList();
 }
 
-function updateItemsList(index){
-    let editedItem = prompt("Please enter your task");
-    tasks.splice(index,1,editedItem);
-    renderList()
+function updateItemsList(index) {
+  let editedItem = prompt("Please enter your task");
+  tasks.splice(index, 1, editedItem);
+  renderList();
 }
